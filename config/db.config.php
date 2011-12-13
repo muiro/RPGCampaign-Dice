a0344d44a109c7491879444d3730226f0b665aea
Dan Austin <wubaga@gmail.com>
2011-12-13 14:33:40 -0500

    'testing'

---
+++ config/db.config.php
@@ -0,0 +1,7 @@
+<?php //dice_db_config.php
+$dice_db_hostname = 'localhost';
+$dice_db_database = 'dice_roll_history';
+$dice_db_username = 'dice_history';
+$dice_db_password = 'donkeyboogermacaroni';
+$dice_db_table = 'dice_roll_history';
+?>
